d76d7b738a23edb8039c035fd51342937619bfb3
Colin Lefter <colin.lefter02@gmail.com>
2023-12-01 18:01:02 -0800

    fixed unique image bug

--- routes/displayImage.js
+++ routes/displayImage.js
@@ -16,8 +16,7 @@ router.get('/', function(req, res, next) {
         try {
             let pool = await sql.connect(dbConfig);
 
-            let sqlQuery = "SELECT imageUrl, productImage FROM product JOIN productImageData ON product.productId = productImageData.productId";
-
+            let sqlQuery = "SELECT imageUrl, productImage FROM product JOIN productImageData ON productImageData.productId = @productId";
             result = await pool.request()
                 .input('productId', sql.Int, idVal)
                 .query(sqlQuery);        
@@ -34,6 +33,7 @@ router.get('/', function(req, res, next) {
                     res.end();
                 } else if (product.imageUrl) {
                     // If no binary image, redirect to the URL
+                    console.log(product.imageUrl);
                     res.redirect(product.imageUrl); // if we have no binary image, we need to redirect to the URL
                 } else {
                     res.status(404).send('Image not available');
