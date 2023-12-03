7ab74b04ad792e3b2d006d5bf2525088d8613c02
Hoc N. <hocng777@gmail.com>
2023-12-02 19:46:44 -0800

    list all customers DONE

--- routes/admin.js
+++ routes/admin.js
@@ -19,14 +19,36 @@ router.get('/', async function(req, res, next) {
             "ORDER BY OrderDate";
         let result = await pool.request().query(sqlQuery);
 
+
+
         let salesData = result.recordset.map(row => ({
             orderDate: row.OrderDate,
             totalOrderAmount: Number(row.TotalOrderAmount).toFixed(2)
         }));
 
+        let sqlQuery2 = "SELECT * FROM customer";
+        let result2 = await pool.request().query(sqlQuery2);
+
+        let customerData = result2.recordset.map(row => ({
+            firstname: row.firstName,
+            lastname: row.lastName,
+            email: row.email,
+            phoneNum: row.phonenum,
+            address: row.address,
+            city: row.city,
+            state: row.state,
+            postalCode: row.postalCode,
+            country: row.country,
+        }));
+
         res.render('admin', {
             title: 'Administrator Sales Report',
-            salesData: salesData
+            salesData: salesData,
+            customerData: customerData
+        });
+        res.render('admin', {
+            title: 'Customers Information',
+            customerData: customerData
         });
 
     } catch(err) {
